38134175906ec3630a5ea1847abaff9eb5d788ab
karla kumako <karlakumako@gmail.com>
2024-05-29 17:49:39 -0300

    se agregan los templates y se suman a los url, se suma parte de las plantillas html para darle imagen a la pagina

--- APPMagic/views.py
+++ APPMagic/views.py
@@ -7,13 +7,16 @@ def home(req):
     return render(req, 'home.html')
 
 def lista_profesores(req):
-    profesores = Profesor.objects.all()
-    return render(req, 'profesores.html', {'profesores': profesores})
+    #profesores = Profesor.objects.all()
+    return render(req, 'Profesor.html', {'profesores': profesores})
 
 def lista_cursos(req):
-    cursos = Curso.objects.all()
-    return render(req, 'cursos.html', {'cursos': cursos})
+    #cursos = Curso.objects.all()
+    return render(req, 'Curso.html', {'cursos': cursos})
 
 def lista_estudiantes(req):
-    estudiantes = Estudiante.objects.all()
-    return render(req, 'estudiantes.html', {'estudiantes': estudiantes})
+    #estudiantes = Estudiante.objects.all()
+    return render(req, 'Estudiante.html', {'estudiantes': estudiantes})
+
+def Casa(req):
+    return render(req, 'Casa.html', {})
